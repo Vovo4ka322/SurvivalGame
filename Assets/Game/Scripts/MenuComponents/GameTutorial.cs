de5a449c00ec21e6f2c0a2b7dc91dd0469ccec19
Vovo4ka322 <vladimir.1979.gdtt.xla@gmail.com>
2025-03-19 10:11:01 +0300

    Fixed buffshop

--- Assets/Game/Scripts/MenuComponents/GameTutorial.cs
+++ Assets/Game/Scripts/MenuComponents/GameTutorial.cs
@@ -12,11 +12,7 @@ namespace Game.Scripts.MenuComponents
         [SerializeField] private Button _continueButton;
         [SerializeField] private RectTransform _abilityInterface;
         [SerializeField] private Button _pauseButton;
-        
-        private readonly bool _isFinished = false;
-        
-        private Coroutine _hideCoroutine;
-        
+
         private void Awake()
         {
             if (_tutorialPanel != null)
@@ -25,7 +21,7 @@ namespace Game.Scripts.MenuComponents
                 Time.timeScale = 0;
             }
         }
-        
+
         private void OnEnable()
         {
             _continueButton.onClick.AddListener(Continue);
@@ -35,51 +31,7 @@ namespace Game.Scripts.MenuComponents
         {
             _continueButton.onClick.RemoveListener(Continue);
         }
-        
-        private void Start()
-        {
-            if (_tutorialPanel != null)
-            {
-                _tutorialPanel.gameObject.SetActive(true);
-                
-                StartCoroutine(HideAfterDelay());
-            }
-        }
-        
-        public void Pause()
-        {
-            if (_hideCoroutine != null)
-            {
-                StopCoroutine(_hideCoroutine);
-                _hideCoroutine = null;
-            }
-            
-            if (_tutorialPanel != null)
-            {
-                _tutorialPanel.gameObject.SetActive(false);
-            }
-        }
 
-        public void Resume()
-        {
-            if(_isFinished)
-            {
-                return;
-            }
-            
-            if (_tutorialPanel != null)
-            {
-                _tutorialPanel.gameObject.SetActive(true);
-                _hideCoroutine = StartCoroutine(HideAfterDelay());
-            }
-        }
-
-        private IEnumerator HideAfterDelay()
-        {
-            yield return new WaitForSeconds(_displayTime);
-            yield return StartCoroutine(_tutorialPanel.FadeOut(_fadeDuration));
-        }
-        
         private void Continue()
         {
             Time.timeScale = 1;
